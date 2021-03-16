7ac2dcd9099f4c281eb06da1f35fb93578c16501
SilvioCresci <silviocresci1996@gmail.com>
2021-03-16 16:38:58 +0100

    nuova chiave

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class DefaultConfig:
 
     PORT = 3978  
     APP_ID = os.environ.get("MicrosoftAppId", "e10038ff-628c-4010-bf43-8176b8f2e0f7")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "vO-x~Nq8eFeOO6kPX6xYiQ4_ZzsQ_us4cw")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "54P-y3l2R._~RK6dC~.Z20RmMGfg.~OaJ9")
     LUIS_APP_ID = os.environ.get("LuisAppId", "80c3205c-c993-4229-a124-d93bfa0a050b")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "08c35c22af9d4eefa96a62a31592ed1f")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
