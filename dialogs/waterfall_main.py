22745f16f6e32434495edf7fa9dd1d124f48ca8e
SilvioCresci <silviocresci1996@gmail.com>
2021-03-18 11:15:14 +0100

    style hero card

--- dialogs/waterfall_main.py
+++ dialogs/waterfall_main.py
@@ -70,7 +70,7 @@ class WaterfallMain(ComponentDialog):
             PromptOptions(
                 prompt=MessageFactory.text(f"Ciao, cosa vuoi fare?"),
                 choices=[Choice("Fattura testuale"), Choice("Fattura visiva"), Choice("Query")],
-                style=ListStyle.hero_card
+                #style=ListStyle.hero_card
             ),
         )  
 
